ed7ad6a4971bd5f687251feb833d736544d0aa32
jackhannah95 <jackhannah01@gmail.com>
2020-05-20 16:42:02 +0100

    package setup

---
+++ R/jafun.R
@@ -0,0 +1,10 @@
+#' \code{jafun} package
+#'
+#' Personal R package of Jack Hannah.
+#'
+#' See the README on
+#' \href{https://github.com/jackhannah95/jafun#readme}{GitHub}.
+#'
+#' @docType package
+#' @name jafun
+NULL
