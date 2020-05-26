a9abc048f0ddfd31aa702d8c7548970219f0a4c1
jackhannah95 <jackhannah01@gmail.com>
2020-05-26 16:58:02 +0100

    use ifelse instead of dplyr::if_else

--- R/day_to_num.R
+++ R/day_to_num.R
@@ -6,19 +6,19 @@
 #' @export
 day_to_num <- function(x) {
   
-  if(any(!x %in% c(days_of_week(), days_of_week(abbr = TRUE)))) {
+  if(any(!x %in% c(jafun::days_of_week(), jafun::days_of_week(abbr = TRUE)))) {
     warning(paste("Entries which do not correspond to a day of the week will", 
                   "return an NA"))
   }
   
-  x <- dplyr::if_else(x %in% c(days_of_week(), days_of_week(abbr = TRUE)),
-                      substr(x, 1, 3),
-                      NA_character_)
+  x <- ifelse(x %in% c(jafun::days_of_week(), jafun::days_of_week(abbr = TRUE)),
+              substr(x, 1, 3),
+              NA_character_)
   
   x <- tibble::enframe(x, name = NULL, value = "day")
   
   y <- tibble::tibble(num = c(1:6, 0),
-                      day = days_of_week(abbr = TRUE))
+                      day = jafun::days_of_week(abbr = TRUE))
   
   x %>%
     dplyr::left_join(y, by = "day") %>%
