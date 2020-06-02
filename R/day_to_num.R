3f8955df9120cec43e0783c9429b1924c8f427f0
jackhannah95 <jackhannah01@gmail.com>
2020-06-02 16:50:34 +0100

    add documentation for day_to_num

--- R/day_to_num.R
+++ R/day_to_num.R
@@ -1,7 +1,27 @@
 #' @title Convert the days of the week to numeric
 #' 
+#' @description \code{day_to_num} converts the days of the week from 
+#' \code{character} format into numeric format for use in \code{\link{n_day}}.
+#' 
+#' @details The \code{Date} and \code{POSIXt} classes within R treat Sunday as 
+#' day 0 in a week, and Saturday as day 6. \code{day_to_num} also treats Sunday 
+#' as day 0 (as opposed to day 7) for the sake of consistency.
+#' 
 #' @param x A character string containing days of the week in full (e.g. 
 #' "Friday") or abbreviated (e.g. "Fri") form.
+#' 
+#' @examples
+#' day_to_num(c("Monday", "Tuesday", "Wed"))
+#' day_to_num(days_of_week())
+#' day_to_num(weekdays(lubridate::dmy(01012020)))
+#' 
+#' @seealso
+#' \code{\link{days_of_week}} for returning a vector of the days of the week.
+#' 
+#' \code{\link[base]{weekdays}} for converting a date into the day of the week.
+#' 
+#' \code{\link{n_day}} for calculating the number of occurrences of a day of 
+#' the week in a set time period.
 #' 
 #' @export
 day_to_num <- function(x) {
