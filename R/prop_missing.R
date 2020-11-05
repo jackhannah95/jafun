be5e8eb7973f374165e6ffb497ed7a201f8cb84e
jackhannah95 <jack.hannah@glasgow.ac.uk>
2020-11-05 16:33:56 +0000

    add error handling to prop_missing

--- R/prop_missing.R
+++ R/prop_missing.R
@@ -5,11 +5,14 @@
 #' @export
 prop_missing <- function(df) {
   
+  if (!inherits(df, "data.frame")) {
+    stop("The input must be a data frame")
+  }
+  
   df %>%
     purrr::map_dfr(~naniar::prop_miss(.x)) %>%
     tidyr::pivot_longer(dplyr::everything(),
                         names_to = "variable",
                         values_to = "prop_missing") %>%
-    dplyr::mutate(prop_missing = scales::percent(.data$prop_missing))
-  
+    dplyr::arrange(dplyr::desc(.data$prop_missing), .data$variable)
 }
