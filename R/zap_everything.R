a58ef307ffb2b50580d69805eeea5e24aa6add36
Jack Hannah <jack.hannah2@phs.scot>
2023-02-08 16:14:44 +0000

    export zap_everything

--- R/zap_everything.R
+++ R/zap_everything.R
@@ -1,12 +1,12 @@
-#' @title Remove all SPSS-related information from data frame
+#' @title Remove all SPSS-related information from a data frame
 #' 
-#' @description \code{.sav} or \code{.zsav} files can be loaded into R using 
-#' \code{\link[haven:read_sav]{haven::read_sav}. This preserves SPSS-related 
+#' @description \code{\link[haven:read_sav]{haven::read_sav}} can be used to 
+#' read\code{.sav} and \code{.zsav} files into R. It preserves SPSS-related 
 #' information such as labels and attributes, which can be removed individually 
 #' using 
 #' \href{https://haven.tidyverse.org/reference/index.html#remove-attributes}{haven's \code{zap_*} functions}.
-#' \code{zap_everything} combines the majority of these functions into a single
-#' step.
+#' \code{zap_everything} combines the majority of these into a single wrapper 
+#' function.
 #' 
 #' @details \code{zap_everything} combines the following \code{haven::zap_*} 
 #' functions:
