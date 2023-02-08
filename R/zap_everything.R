187c5362ac7b5733b846964d01df3a00b7f51a32
Jack Hannah <jack.hannah2@phs.scot>
2023-02-08 11:31:39 +0000

    add zap_empty

--- R/zap_everything.R
+++ R/zap_everything.R
@@ -5,6 +5,14 @@ zap_everything <- function(df) {
     haven::zap_label() %>%
     haven::zap_labels() %>%
     haven::zap_formats() %>%
-    haven::zap_widths()
+    haven::zap_widths() %>%
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::vars_select_helpers$where(
+          is.character,
+          haven::zap_empty
+        )
+      )
+    )
 }
 
